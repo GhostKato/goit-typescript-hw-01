b206dae224f652a8fccc83e6da66ba7c109df72d
GhostKato <osadchenko.maksym@gmail.com>
2024-08-24 18:53:41 +0300

    commit4

--- src/generics/3.ts
+++ src/generics/3.ts
@@ -1,4 +1,4 @@
-function merge<T, U>(objA: T, objB: U): T & U {
+function merge<T extends object, U extends object>(objA: T, objB: U): T & U {
   return Object.assign({}, objA, objB);
 }
 
@@ -8,3 +8,4 @@ const objB = { city: 'Odessa', occupation: 'Engineer' };
 const mergedObject = merge(objA, objB);
 
 console.log(mergedObject);
+
